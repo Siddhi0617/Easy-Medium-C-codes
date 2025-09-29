bc3fa505c4a9cf9666a37c9a20c796a3bc6b0eae
Siddhi Shrungare <siddhishrungare6@gmail.com>
2025-09-29 10:21:03 -0700

    Add files via upload

---
+++ color.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    char color[50];
+
+    printf("Enter your Favourite Colour: ");
+    scanf("%s", color);   
+
+    printf("Your Favourite Colour is %s\n", color);
+    
+}
