20e93d31487c60c20d07f9001f9fdcb9f7590524
Siddhi Shrungare <siddhishrungare6@gmail.com>
2025-10-20 09:59:24 -0700

    Add files via upload

---
+++ usingfunctionsquare.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void printSquare(int n) 
+{
+    for(int i=0; i<n; i++)
+     {
+        for(int j=0; j<n; j++)
+            printf("* ");
+        printf("\n");
+    }
+}
+
+int main()
+ {
+    int n;
+    printf("Enter size: ");
+    scanf("%d", &n);
+    printSquare(n);
+    return 0;
+}
